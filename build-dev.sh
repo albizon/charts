f8a22693962f2ce615409010e2275ba4cd6882f3
Alexis BIZON <alexis.bizon@biz-cho.ovh>
2020-06-02 19:39:57 +0200

    Preservation des aciens charts

--- build-dev.sh
+++ build-dev.sh
@@ -1,8 +1,12 @@
 for d in ./* ; do
         FILE=$d/Chart.yaml
+	FILE_BUILD=$d/build.sh
         if test -f "$FILE"; then
-                cd $d
-                bash ./build.sh
-                helm package ./ -d files-to-gh-pages/dev
+		if test -f "$FILE_BUILD"; then
+			cd $d
+                	bash ./build.sh
+			cd ..
+		fi
+                helm package $d -d files-to-gh-pages/dev
         fi
 done
