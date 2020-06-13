a9e2ee3219e34c082ee6be8f32bd92873e3c546e
Alexis BIZON <alexis.bizon@biz-cho.ovh>
2020-06-13 19:40:56 +0200

    Add pr-dev script

---
+++ build-pr-dev.sh
@@ -0,0 +1,18 @@
+for d in ./* ; do
+        FILE=$d/Chart.yaml
+	FILE_BUILD=$d/build.sh
+        if test -f "$FILE"; then
+		if test -f "$FILE_BUILD"; then
+			cd $d
+                	bash ./build.sh
+			cd ..
+		fi
+		cat $FILE
+#                helm repo add   stable https://kubernetes-charts.storage.googleapis.com/
+#                helm repo update
+                helm dep build $d
+		ls -l $d
+		ls -l $d/charts
+		helm package $d -d files-to-gh-pages/dev
+        fi
+done
